60006a993c0f1c37869735e244dceb3bb1338104
module3_last2 <unisport2006@mail.ru>
2024-10-02 08:43:29 +0300

    Add files via upload

---
+++ model6_2.py
@@ -0,0 +1,48 @@
+class Vehicle:
+    COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self,__model, __engine_power,__color,owner):
+        self.__color=__color
+        self.owner=owner
+        self.__model=__model
+        self.__engine_power= __engine_power
+        self.set_color(__color)
+    def get_model(self,__model:str):
+       #self.__model=__model
+        return print(f'Модель: {__model}')
+
+    def get_horsepower(self,__engine_power:int):
+        #self.__engine_power=__engine_power
+        return print(f"Мощность двигателя: {__engine_power}")
+
+    def get_color(self,__color:str):
+        #self.__color=__color
+        self.set_color(__color)
+
+        return print(f'Цвет: {__color}')
+
+    def set_color(self,__color:str):
+        __color=__color.lower()
+        if __color not in self.COLOR_VARIANTS:
+            new_color = __color
+            print(f'Нельзя сменить цвет на {new_color}')
+
+    def owner(self,owner:str):
+        return print(f'Владелец: {owner}')
+
+    def print_info (self,__model:str,__engine_power:int,__color:str,owner:str):
+        return print(f"Mарка:{__model},  Мощность двигателя:{__engine_power},  Цвет:{__color},  Владелец:{owner}")
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+    pass
+
+vehicle1 = Sedan("Toyta Mark II",500,"blue","Fedos" )
+vehicle1.print_info("Toyta Mark II",500,"blue","Fedos" )
+vehicle1.set_color("Pink")
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+print(vehicle1.owner)
+vehicle1.print_info('Toyta Mark II',500,'BLAK','Vasyok')
+
