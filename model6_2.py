b2c70a818fe91cb1166f499b6d3f9253e9a32dec
module3_last2 <unisport2006@mail.ru>
2024-10-02 19:23:25 +0300

    Update model6_2.py

--- model6_2.py
+++ model6_2.py
@@ -1,38 +1,31 @@
 class Vehicle:
     COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
-    def __init__(self,__model, __engine_power,__color,owner):
-        self.__color=__color
+    def __init__(self,model:str, engine_power:int,color:str,owner:str):
+        self.__color=color
         self.owner=owner
-        self.__model=__model
-        self.__engine_power= __engine_power
-        self.set_color(__color)
-    def get_model(self,__model:str):
-       #self.__model=__model
-        return print(f'Модель: {__model}')
+        self.__model=model
+        self.__engine_power= engine_power
+        self.set_color(color)
 
-    def get_horsepower(self,__engine_power:int):
-        #self.__engine_power=__engine_power
-        return print(f"Мощность двигателя: {__engine_power}")
+    def get_model(self,model):
+        return f'Модель: {model}'
 
-    def get_color(self,__color:str):
-        #self.__color=__color
-        self.set_color(__color)
+    def get_horsepower(self,engine_power):
+        return f"Мощность двигателя: {engine_power}"
 
-        return print(f'Цвет: {__color}')
+      def get_color(self,new_color):
+        return (f'Цвет: {new_color}')
 
-    def set_color(self,__color:str):
-        __color=__color.lower()
-        if __color not in self.COLOR_VARIANTS:
-            new_color = __color
-            print(f'Нельзя сменить цвет на {new_color}')
+    def set_color(self,color):
+        new_color=color.lower()
+        if new_color in self.COLOR_VARIANTS:
+            self.color = new_color
+        else:
+            print(f' Нельзя сменить цвет на {color}')
 
-        def get_owner(self,owner:str):
-        return print(f'Владелец: {self.owner}')
-
-    def print_info (self,__model:str,__engine_power:int,__color:str,owner:str):
-         print(f"{self.get_model(__model)}, {self.get_horsepower(__engine_power)}, {self.get_color(__color)}, {self.get_owner(owner)}")
-
+    def print_info (self,__model,__engine_power,__color,owner):
+        print(f" {self.get_model(__model)}\n {self.get_horsepower(__engine_power)}\n {self.get_color(__color)}\n Владелец: {owner}")
 
 class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
