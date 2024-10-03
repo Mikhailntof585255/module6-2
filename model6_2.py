5e0659573d4accad8c5c751602605e24c995f20d
module3_last2 <unisport2006@mail.ru>
2024-10-03 19:46:13 +0300

    Update model6_2.py

--- model6_2.py
+++ model6_2.py
@@ -1,42 +1,36 @@
-class Vehicle:
+class  Vehicle:
     COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
-
-    def __init__(self,model:str, engine_power:int,color:str,owner:str):
+    def __init__(self,model:str,engine_power:int,color:str,owner:str):
         self.__color=color
-        self.owner=owner
         self.__model=model
-        self.__engine_power= engine_power
-        self.set_color(color)
+        self.__engine_power = engine_power
+        self.owner = owner
 
-    def get_model(self,model):
-        return f'Модель: {model}'
-
-    def get_horsepower(self,engine_power):
-        return f"Мощность двигателя: {engine_power}"
-
-      def get_color(self,new_color):
-        return (f'Цвет: {new_color}')
-
-    def set_color(self,color):
-        new_color=color.lower()
-        if new_color in self.COLOR_VARIANTS:
-            self.color = new_color
+    def get_model(self):
+        return f'Модель: {self.__model}'
+    def get_horsepower(self):
+        return f"Мощность двигателя: {self.__engine_power}"
+    def get_color(self):
+        return f'Цвет: {self.__color}'
+    def set_color(self,__color):
+        new_color = __color
+        if new_color.lower() in self.COLOR_VARIANTS:
+            self.__color = new_color
+            print()
         else:
-            print(f' Нельзя сменить цвет на {color}')
+            print(f'Нельзя сменить цвет на {new_color}')
+    def print_info(self):
+        return print(f'{self.get_model()}\n{self.get_horsepower()}\n{self.get_color()}\nВладелец: {self.owner}')
 
-    def print_info (self,__model,__engine_power,__color,owner):
-        print(f" {self.get_model(__model)}\n {self.get_horsepower(__engine_power)}\n {self.get_color(__color)}\n Владелец: {owner}")
 
 class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
 
-    pass
 
 vehicle1 = Sedan("Toyta Mark II",500,"blue","Fedos" )
-vehicle1.print_info("Toyta Mark II",500,"blue","Fedos" )
-vehicle1.set_color("Pink")
+vehicle1.print_info()
+vehicle1.set_color('Pink')
 vehicle1.set_color('BLACK')
-vehicle1.owner = 'Vasyok'
-print(vehicle1.owner)
-vehicle1.print_info('Toyta Mark II',500,'BLAK','Vasyok')
-
+vehicle1.owner='Vasyok'
+vehicle1.print_info()
+         
